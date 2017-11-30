58f619ef4e22f7e658140de58509f8bcdb6a25a4
steinikarls <steinikarls@live.com>
2017-11-30 22:30:01 +0000

    show function and fixes
    implemented cell prototype show function for the grid and tidy up the text in sketch regarding size

--- VEF2B3U/Lokaverkefni/sketch.js
+++ VEF2B3U/Lokaverkefni/sketch.js
@@ -13,17 +13,19 @@ function make2DArray(cols, rows) {
 
 
 var grid;
-var cols = 20;
-var rows = 20;
-
+var cols;
+var rows;
+var w = 20;
 
 
 function setup() {
 	createCanvas(200, 200);
+	cols = floor(width / w);
+	rows = floor(height / w);
 	grid = make2DArray(cols, rows);
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
-			grid[i][j] = new Cell();
+			grid[i][j] = new Cell(i * w, j * w, w);
 		}
 	}
 
