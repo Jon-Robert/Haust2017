3c67cf316ba398087c24f2de9f63a0c6bb17e45c
steinikarls <steinikarls@live.com>
2017-12-01 08:26:08 +0000

    minor fix
    when pressing a specific box the whole y colum shows all bees
    
    fixed that with y insted of this.y

--- VEF2B3U/Lokaverkefni/cell.js
+++ VEF2B3U/Lokaverkefni/cell.js
@@ -30,7 +30,7 @@ Cell.prototype.show = function() {
 
 
 Cell.prototype.contains = function(x, y) {
-	return (x > this.x && x < this.x + this.w && y > this.y && this.y < this.y + this.w);
+	return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
 };
 
 Cell.prototype.reveal = function() {
