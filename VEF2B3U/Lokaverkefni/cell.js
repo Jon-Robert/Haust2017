331886d00df783b32b4dad1393e4944df7686390
steinikarls <steinikarls@live.com>
2017-12-04 03:23:50 +0000

    comments
    comments

--- VEF2B3U/Lokaverkefni/cell.js
+++ VEF2B3U/Lokaverkefni/cell.js
@@ -6,31 +6,42 @@
 function Cell(i, j, w) {
 	this.i = i;
 	this.j = j;
-	this.x = i*w;
-	this.y = j*w;
+	this.x = i*w; // reikna pixel location
+	this.y = j*w; // reikna pixel location
 	this.w = w;
+	//  total varible fyrir neighbur í countbees
 	this.neighborCount = 0;
-	
+	// hvort það er bee í reitinum
 	this.bee = false;
+	// að sýna ekki innihaldið á reitinum
 	this.revealed = false;
 }
 
 
-
+// prototype sýna 
 Cell.prototype.show = function() {
 	stroke(0);
 	noFill();
+	// bý til hring 
 	rect(this.x, this.y, this.w, this.w);
+	// reiturinn er sýndur 
 	if (this.revealed) {
+		// ef það er bee 
 		if (this.bee) {
+			//læt bee vera ljósgrár
 			fill(200);
+			// teikna ég hring til að segja að það er bee 
 			ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
 		} else {
+			// annars teikna ljósgráan kassa 
 			fill(200);
 			rect(this.x, this.y, this.w, this.w);
+			// aðeins skrifa ef nágranni er meira en 0
 			if (this.neighborCount > 0) {	
+				// text í center á reiti
 				textAlign(CENTER);
 				fill(0);
+				// skrifa fjöldi nágranna og laga staðsetningu
 				text(this.neighborCount, this.x +this.w * 0.5, this.y + this.w - 6);
 			}
 		}
@@ -38,49 +49,64 @@ Cell.prototype.show = function() {
 	
 };
 
-
+// telja nágranna
 Cell.prototype.countBees = function() {
+	// ef það er bee þá skila -1
 	if (this.bee) {
 		this.neighborCount = -1;
 		return;
 	}
 	var total = 0;
-
+	// fer yfir alla nágranna
 	for (var xoff = -1; xoff <= 1; xoff++) {
 		for (var yoff = -1; yoff <=  1; yoff++) {
 			var i = this.i + xoff;
 			var j = this.j + yoff;
+			// fyrir offset ef hann er 0,0 það og hann tékkar hvort það er reitur uppi og til hliðar ef ekki það ekkert ef það er þá heldur hann áfram / offset
 			if (i > -1 && i < cols && j > -1 && j < rows) {
+				// þetta er allir nágrannar sem ég vill skoða
 				var neighbor = grid[i][j];
+				// ef nágranni hefur bee á að bæta total
 				if (neighbor.bee) {
 					total++;
 				}
 			}	
 		}	
 	}
-		this.neighborCount = total;
+	// skila total
+	this.neighborCount = total;
 };
-
+// hvort innihald þessa cell punktur
 Cell.prototype.contains = function(x, y) {
+	// er x og y á milli x1 og x2 og y1 og y2 
+	// og skila 
 	return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
 };
-
+// sýna reitinn
 Cell.prototype.reveal = function() {
+	//geri reitinn sýnilegann
 	this.revealed = true;
+	// ef það er enginn nágrannar
 	if (this.neighborCount == 0) {
+		// starta function floodfill
 		this.floodFill();
 
 	}
 };
 
+// flooofill function
 Cell.prototype.floodFill = function() {
+	// fer yfir alla nágranna
 	for (var xoff = -1; xoff <= 1; xoff++) {
 		for (var yoff = -1; yoff <=  1; yoff++) {
 			var i = this.i + xoff;
 			var j = this.j + yoff;
+			// fyrir offset ef hann er 0,0 það og hann tékkar hvort það er reitur uppi og til hliðar ef ekki það ekkert ef það er þá heldur hann áfram / offset
 			if (i > -1 && i < cols && j > -1 && j < rows) {
 				var neighbor = grid[i][j];
+				// ef neighbor er ekki bee og ekki hefur verið sýnt annars infitive loop í gangi
 				if (!neighbor.bee && !neighbor.revealed) {
+					// sýna það
 					neighbor.reveal();
 				}
 			}	
--- VEF2B3U/Lokaverkefni/sketch.js
+++ VEF2B3U/Lokaverkefni/sketch.js
@@ -1,80 +1,102 @@
 "use strict";
+// tengi p5 library við javascriptið mitt
 new p5();
 
+// geri 2darray sem tekur colums og rows
 function make2DArray(cols, rows) {
+	// by til array sem hefur viss mikið af colums
 	var arr = new Array(cols);
+	//fyrir hvert colum geri það array með viss mikið af rows
 	for (var i = 0; i < arr.length; i++) {
 		arr[i] = new Array(rows);
 	}
+	// skila array
 	return arr;
 }
 
 
 
-
+// geri varibles
 var grid;
 var cols;
 var rows;
+// hver reitur er 20px
 var w = 20;
+// fjöldi bees
 var totalBees = 50;
 
 function setup() {
+	// bý til canvas sem er 401px 
 	createCanvas(401, 401);
+	// tek breidd og deili með reiti/w til að fá x margar cols og rows og floor er til að hafa slétta tölu
 	cols = floor(width / w);
 	rows = floor(height / w);
+	// sem er viss stórt og nota cols og rows varible sem stærðinn
 	grid = make2DArray(cols, rows);
+	// geri forlykkju sem býr til grid sem er 20 x 20 stórt
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
+			// bý til hvern reit inni í grid sem sendir gögn í cell function
 			grid[i][j] = new Cell(i, j, w);
 		}
 	}
 
-
+	// er array
 	var options = [];
+	// fer í gegnum allt grid
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
+			// og set alla möguleika staðsetningar inn í það
 			options.push([i,j]);
 		}
 	}
-
-
 	
-  for (var n = 0; n < totalBees; n++) {
-    var index = floor(random(options.length));
-    var choice = options[index];
-    var i = choice[0];
-    var j = choice[1];
-    // Deletes that spot so it's no longer an option
-    options.splice(index, 1);
-    grid[i][j].bee = true;
-  }
+
+	// velja totalbees staðsetningar
+  	for (var n = 0; n < totalBees; n++) {
+  		// að gefa mér random option
+	    var index = floor(random(options.length));
+	    //það sem eg er að velja úr option
+	    var choice = options[index];
+	    // gefur mér x og y staðsetningu
+	    var i = choice[0];
+	    var j = choice[1];
+	    // fjarlægi staðsetningu svo það er ekki enn möguleiki
+	    options.splice(index, 1);
+	    // geri það að bee með x og y staðsetningu
+	    grid[i][j].bee = true;
+  	}
 
 
-
+  // fer yfir alla reiti og tel hversu marga bee nágranna þeir hafa
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
+			// function til að telja nágranna 
 			grid[i][j].countBees();
 		}
 	}
 	
 }
-
+// function leikur lokið
 function gameOver() {
+	// renn í gegnum allt grid 
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
+			// sýni allt saman
 			grid[i][j].revealed = true;
 		}
 	}
 }
 
-
-
+// ýta á mús
 function mousePressed() {
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
+			// tek staðsetningu og athuga hvaða reit ég ýtti á sýni svo rétta reit
 			if (grid[i][j].contains(mouseX, mouseY)) {
+				// sýni rétta reit sem ýtt var á 
 				grid[i][j].reveal();
-			
+				// ef það er bee rennur function sem sýnir allt og leikur þá búinn
 				if (grid[i][j].bee) {
 					gameOver();
 				}
@@ -87,7 +109,9 @@ function mousePressed() {
 
 
 function draw() {
+	// bakgrunnur hvítur
 	background(255);
+	// sýna hvað er í cells
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
 			grid[i][j].show();
